6791d7ceaefe4c0ee029e685a76db3df1dac2ac0
github username <github email address>
2020-04-11 09:19:38 -0700

    sessions controller/ genre view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+    def welcome
+    end
+    
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+   
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+            if @user.try(:authenticate, params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:error] = "Sorry, login info was incorrect. Please try again."
+                redirect_to login_path
+            end
+    end
+
+   def omniauth
+        @user = User.find_or_create_by(email: auth[:info][:email]) do |f|
+            f.password = SecureRandom.hex
+        end
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+   end
+
+private
+
+    def auth
+         request.env['omniauth.auth']
+    end
+
+end
